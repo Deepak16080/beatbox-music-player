48925e00c547f90954ecbdf68e34a69fa8037a51
Gaurav <269mehta@gmail.com>
2024-02-18 18:37:04 +0530

    Feat: Fetching Album Tracks

--- lib/data/apiClient/api_client.dart
+++ lib/data/apiClient/api_client.dart
@@ -2,46 +2,20 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:beatbox/data/models/deezer_resp.dart';
 import 'package:http/http.dart' as http;
 
 final apiurl = 'https://api.deezer.com/album/302127';
 
 class MusicAPI {
-  FutureOr<Album> fetchAlbum() async {
+  Future<List<Track>> fetchAlbumTracks() async {
     final response = await http.get(Uri.parse(apiurl));
     final Map<String, dynamic> responseData = json.decode(response.body);
-    final album = Album.fromJson(responseData);
+    final album = DeezerAlbumResponse.fromJson(responseData);
     if (response.statusCode == 200) {
-      print('Image URL: ${album.imageUrl}');
-      print('Title: ${album.title}');
-      print('Artist: ${album.artistName}');
-
-      return album;
+      return album.tracks?.data ?? [];
     } else {
       throw Exception('Failed to load album');
     }
-  }
-}
-
-class Album {
-  final String title;
-  final String artistName;
-  final String imageUrl;
-  final String songPath;
-
-  Album({
-    required this.title,
-    required this.artistName,
-    required this.imageUrl,
-    required this.songPath,
-  });
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      title: json['title'],
-      artistName: json['artist'] != null ? json['artist']['name'] ?? '' : '',
-      imageUrl: json['cover_big'] ?? '',
-      songPath: json['preview'] ?? '',
-    );
   }
 }
